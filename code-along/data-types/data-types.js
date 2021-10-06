0778b72163e22bbc6fae6e98670a799247df2a5e
martybonacci <martybonacci@gmail.com>
2021-10-06 09:17:00 -0600

    added functions code along

---
+++ code-along/data-types/data-types.js
@@ -0,0 +1,17 @@
+// string
+const name = 'Fred'
+const anotherName = "Jim"
+const yetAnotherName = `Mary and ${name}`
+
+// number
+const age = 18
+
+const zipcode = '87109'
+
+const cars = ['ford', 'dodge', 'toyota']
+const empty = []
+console.log(cars[0])
+console.log(cars[2])
+
+// Object ....JSON
+const person = {name: 'Fred', age: 18}
---
+++ code-along/functions-1/functions-1.js
@@ -0,0 +1,5 @@
+// Function #1
+// Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
+
+// 	Function #2
+// Take two strings as arguments and return a value that has them concatenated.
