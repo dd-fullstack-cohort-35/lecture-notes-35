2b3fdc67347ece8930787654064c1b1946eee497
martybonacci <martybonacci@gmail.com>
2021-10-06 10:01:20 -0600

    added more functions code along

--- code-along/functions-1/functions-1.js
+++ code-along/functions-1/functions-1.js
@@ -1,5 +1,20 @@
 // Function #1
 // Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
 
+function square(root) {
+	return root * root;
+}
+console.log(square(5))
+
+function subtract(number1, number2){
+	const difference = number1 - number2
+	return difference
+}
+
+console.log('margin ', subtract(2000,1000))
+console.log('profit ', subtract(1000,500))
+console.log('doing it without the function', 2000-1000)
+
+
 // 	Function #2
 // Take two strings as arguments and return a value that has them concatenated.
