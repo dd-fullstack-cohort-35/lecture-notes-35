13c2347fb9f006da02178a4a90d7c18ba7e48a37
martybonacci <martybonacci@gmail.com>
2021-10-08 08:56:22 -0600

    array of objects

--- code-along/JSON-objects/JSON-objects.js
+++ code-along/JSON-objects/JSON-objects.js
@@ -14,3 +14,23 @@ console.log(person.age)
 person.run()
 person.age = person.age + 1
 console.log(person.age)
+
+// const walk = function(){console.log('walking slow')}
+// function walk (){console.log('walking slow')}
+
+
+let anotherPerson = {
+	name: 'Sue',
+	age: 27,
+	hairColor: 'brown',
+	run: function(){console.log('walking')}
+}
+console.log('another person hair color ', anotherPerson.hairColor)
+anotherPerson.run()
+
+// array of 2 people objects
+let people = [person, anotherPerson,{ name: 'Mary', age: 32, hairColor: 'pink', run: function(){console.log('walking fast')} }]
+console.log(people[0])
+console.log(people)
+console.log(people[2])
+console.log(people[2].name + ' has ' + people[2].hairColor + ' hair.')
