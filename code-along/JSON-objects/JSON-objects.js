8c4bf55d9fecfe086b9176320ecac23fa7f7b39f
martybonacci <martybonacci@gmail.com>
2021-10-08 08:32:25 -0600

    objects code along comments

--- code-along/JSON-objects/JSON-objects.js
+++ code-along/JSON-objects/JSON-objects.js
@@ -1,14 +1,16 @@
 const myArray = ['Jim', 28, 'red']
 console.log(myArray[2])
 
+// objects have key/value pairs
 let person = {
 	name: 'Jim',
 	age: 28,
 	hairColor: 'red',
 	run: function(){console.log('running')}
 }
-
+// We access object's state values with a dot
 console.log(person.age)
+// objects can contain functions also know as methods
 person.run()
 person.age = person.age + 1
 console.log(person.age)
