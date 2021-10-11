7fab879cec532f606a951639ca38c353af5b0230
martybonacci <martybonacci@gmail.com>
2021-10-11 09:18:03 -0600

    added arrow-functions.js

---
+++ code-along/functions-2/arrow-functions.js
@@ -0,0 +1,17 @@
+
+function myFunction (argument){
+	console.log(argument)
+}
+
+myOtherFunction = function(argument){
+	console.log(argument)
+}
+
+myArrowFunction = (argument) => {
+	console.log(argument)
+}
+
+myShorterArrowFunction = argument => console.log(argument)
+
+
+
--- code-along/functions-2/functions-2.js
+++ code-along/functions-2/functions-2.js
@@ -29,4 +29,4 @@ console.log(convertWeightToGrams(2, 'buckets'))
 
 
 
-	// Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number.  (The factors of a number are all numbers that divide evenly into it.  For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
+	// Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number. (The factors of a number are all numbers that divide evenly into it. For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
