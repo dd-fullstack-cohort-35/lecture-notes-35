e1640424fa386c1e8584145c05a606a257897e7e
martybonacci <martybonacci@gmail.com>
2021-10-11 09:41:50 -0600

    added second function in functions-2.js

--- code-along/functions-2/functions-2.js
+++ code-along/functions-2/functions-2.js
@@ -21,12 +21,14 @@ function convertWeightToGrams (weight, units){
 console.log(convertWeightToGrams(1, 'lbs'))
 console.log(convertWeightToGrams(2, 'buckets'))
 
-
-
-
-
-
-
-
-
 	// Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number. (The factors of a number are all numbers that divide evenly into it. For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
+sumOfFactors = (number) => {
+	let sum = 0
+	for (let i = 1; i <= number; i++){
+		if (number % i === 0){
+			sum = sum + i
+		}
+	}
+	return sum
+}
+console.log(sumOfFactors(12))
