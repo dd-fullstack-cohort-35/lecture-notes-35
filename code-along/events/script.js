484783c71e374199f4a73356877c370857ac993e
Marlon Oliver Peseke <mpeseke@gmail.com>
2021-10-21 11:26:10 -0600

    added javascript events code along

---
+++ code-along/events/script.js
@@ -0,0 +1,31 @@
+function createMonsterCell(data) {
+    console.log(data);
+    const monsterCell = document.createElement('div');
+
+
+    monsterCell.classList.add("monster-cell");
+
+
+
+    const paragraph = document.createElement('p');
+    const node = document.createTextNode(data.name.charAt(0).toUpperCase() + data.name.slice(1));
+
+    const img = document.createElement('img');
+    img.src = data.sprites.front_default;
+
+    paragraph.appendChild(node);
+    monsterCell.append(paragraph, img);
+
+
+    const element = document.getElementById('monsters');
+
+    element.appendChild(monsterCell);
+}
+
+function fetchData() {
+    const rand = Math.floor(Math.random()*898)
+
+    fetch('https://pokeapi.co/api/v2/pokemon/'+rand)
+        .then(response => response.json())
+        .then(json => createMonsterCell(json));
+}
