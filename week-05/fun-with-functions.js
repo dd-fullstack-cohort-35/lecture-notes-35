5adce0f36e5d5a71b1e9361c87b8502df3724603
Marlon Oliver Peseke <mpeseke@gmail.com>
2021-11-03 16:18:24 -0600

    changes

--- week-05/fun-with-functions.js
+++ week-05/fun-with-functions.js
@@ -9,3 +9,23 @@ function sayAnotherMessage(){
 	console.log('Water is wet.')
 }
 
+sayAnotherMessage();
+
+// const greetUser = (userName) => {
+// 	console.log(`Hello, ${userName}!`);
+// }
+//
+// const greetUserName = () => {
+// 	let user = prompt("What's your name?")
+//
+// 	greetUser(user);
+// }
+//
+// greetUserName()
+
+function addTwoNumbers(num1, num2) {
+	console.log(num1 + num2)
+
+}
+
+addTwoNumbers(40, "a");
