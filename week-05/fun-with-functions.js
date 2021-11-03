1f5ab391e72f6715397193b2dd07ca4147c79afa
martybonacci <martybonacci@gmail.com>
2021-11-03 14:08:04 -0600

    added fun-with-functions.js

---
+++ week-05/fun-with-functions.js
@@ -0,0 +1,11 @@
+// define function to use later
+const sayMessage = function(){
+	console.log('I like turtles.')
+}
+
+sayMessage()
+
+function sayAnotherMessage(){
+	console.log('Water is wet.')
+}
+
