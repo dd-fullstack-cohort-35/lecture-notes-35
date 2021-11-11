aeea6228b6d0b10c6ba7601ba340556c6c2b37d8
Marlon Oliver Peseke <mpeseke@gmail.com>
2021-11-11 10:07:00 -0700

    added a closure

--- week-06/intro-to-typescript/example/hello.world.ts
+++ week-06/intro-to-typescript/example/hello.world.ts
@@ -13,7 +13,13 @@ class Greeter {
 let greeter = new Greeter("Carlos");
 console.log(greeter.greet());
 
-class Pet {
-    constructor(id: number, type: string, tail: boolean) {
+
+function makeAdder(var1: number) {
+    return function(var2: number): number {
+        return var1 + var2
     }
 }
+
+const add10 = makeAdder(10)
+
+console.log(add10(5))
